9c402cedabf61c871fa259ff24d204a517900a02
Crowdbotics <team@crowdbotics.com>
2021-08-06 19:03:29 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "nameless_limit_29581Identifier",
+  url: "https://nameless_limit_29581Identifier.botics.co",
+  api: "https://nameless_limit_29581Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
